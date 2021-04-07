ae4d0c7243ed7fea24819d62bf99fbd8a21d205b
Mcode0105 <munifabdul603@gmail.com>
2021-04-07 21:04:43 +0700

    first commit

---
+++ app/Views/admin/mahasiswa.php
@@ -0,0 +1,43 @@
+<?= $this->extend('layout/template'); ?>
+<?= $this->section('content'); ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <h2> Data Mahasiswa</h2>
+            <!-- DataTales Example -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Nim</th>
+                                    <th>Alamat</th>
+                                    <th>Foto</th>
+                                    <th>Opsi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Tiger Nixon</td>
+                                    <td>System Architect</td>
+                                    <td>Edinburgh</td>
+                                    <td>61</td>
+                                    <td>2011/04/25</td>
+                                    <td>$320,800</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(''); ?>
